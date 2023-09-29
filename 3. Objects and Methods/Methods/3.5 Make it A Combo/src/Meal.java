f54db0a1bf76de550d7cb53972bc63b9a4322731
Miguel Cabeza <migcab86@gmail.com>
2023-09-29 18:33:46 +0100

    first commit

---
+++ 3. Objects and Methods/Methods/3.5 Make it A Combo/src/Meal.java
@@ -0,0 +1,25 @@
+public class Meal
+{
+    //Instance attributes//
+    private String name;
+    private double cost;
+
+    //Class constructor//
+    public Meal(String food, double price)
+    {
+        name = food;
+        cost = price;
+    }
+
+    public void makeItACombo()
+    {
+        name += " with fries and soda";
+        cost += 3;
+    }
+
+    public String getBill()
+    {
+        return name + ", $" + cost;
+    }
+
+}
