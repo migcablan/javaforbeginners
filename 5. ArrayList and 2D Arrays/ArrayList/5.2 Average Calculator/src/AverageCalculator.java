b980f5f5c31f911f6a6c74e27215cf705a478701
Miguel Cabeza <migcab86@gmail.com>
2023-09-29 18:37:27 +0100

    commit final

--- 5. ArrayList and 2D Arrays/ArrayList/5.2 Average Calculator/src/AverageCalculator.java
+++ 5. ArrayList and 2D Arrays/ArrayList/5.2 Average Calculator/src/AverageCalculator.java
@@ -4,6 +4,11 @@ public class AverageCalculator
 {
     public static double getAverageOf (ArrayList<Integer> numbers)
     {
-        /* TODO */
+        double sum = 0;
+        for (int number: numbers)
+        {
+            sum += number;
+        }
+        return sum/numbers.size();
     }
 }
